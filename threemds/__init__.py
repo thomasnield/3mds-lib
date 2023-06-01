e8945b52a9eb09c6a8a38754c4a92254af50e543
Thomas Nield <thomasnield@live.com>
2023-05-31 22:20:48 -0500

    add ppt template, more bug fixes

--- threemds/__init__.py
+++ threemds/__init__.py
@@ -9,8 +9,8 @@ __all__ = ["create_svg_from_vgroup", "create_svg_from_vmobject"]
 VMobject.to_svg = create_svg_from_vmobject
 VGroup.to_svg = create_svg_from_vgroup
 
-def create_svg_from_scene(scene: Scene, file_name: str | Path = None):
-    return VGroup(*scene.mobjects).to_corner(UL).to_svg(file_name)
+def create_svg_from_scene(scene: Scene, file_name: str | Path = None, trim=True):
+    return VGroup(*scene.mobjects).to_svg(file_name, trim=trim)
 
 Scene.to_svg = create_svg_from_scene
 
--- threemds/utils.py
+++ threemds/utils.py
@@ -1,5 +1,6 @@
 import re, os, sys
 
+scene_regex = r"(?<=^class )[A-Za-z]+(?=\([A-Za-z]*Scene\))"
 def load_file(filepath):
     file = open(filepath, mode='r')
     text = file.read()
@@ -13,9 +14,15 @@ def findall(regex, str=None, file=None):
     return re.findall(regex, str, re.MULTILINE)
 
 def render_scenes(q="l", play=False, gif=False, scene_names=None):
-    regex = r"(?<=^class )[A-Za-z]+(?=\([A-Za-z]*(Scene|Slide)\))"
     f = sys.argv[0]
-    for i,scene_name in enumerate(findall(regex,file=f)):
+    for i,scene_name in enumerate(findall(scene_regex,file=f)):
+        print(scene_name)
         if scene_names is None or scene_name in scene_names:
            print(f"Rendering: {scene_name}")
            os.system(f"manim -q{q} -v WARNING --disable_caching {'-p' if play else ''} {'--format=gif' if gif else ''} -o {i+1:02d}_{scene_name} {sys.argv[0]} {scene_name}")
+
+def render_slides():
+    regex = r"(?<=^class )[A-Za-z]+(?=\(Slide\))"
+    f = sys.argv[0]
+
+    os.system(f"manim-slides {' '.join(findall(regex,file=f))}")
